ca67ff729597530922352a4058e5c73176fb80a7
Johannes Leupolz <johannes.leupolz@googlemail.com>
2017-07-22 16:05:03 +0200

    restart pulseaudio when it stops

--- setup_raspberry_client.sh
+++ setup_raspberry_client.sh
@@ -24,17 +24,29 @@ cp * -Rf /home/audioclient/bluetooth-monitor
 cp /home/audioclient/bluetooth-monitor/example-config.yaml /home/audioclient/bluetooth-monitor/config.yaml
 chown -R audioclient:audioclient /home/audioclient/
 
-#enable dbus and pulseaudio for user
+
 mkdir -p /home/audioclient/.config/systemd/user/sockets.target.wants
-ln -s /usr/lib/systemd/user/dbus.socket /home/audioclient/.config/systemd/user/sockets.target.wants
-ln -s /usr/lib/systemd/user/pulseaudio.socket /home/audioclient/.config/systemd/user/sockets.target.wants
 mkdir -p /home/audioclient/.config/systemd/user/default.target.wants
+
+# enable dbus for user
+# ln -s /usr/lib/systemd/user/dbus.socket /home/audioclient/.config/systemd/user/sockets.target.wants # should not be necessary with package dbus-user-session installed
+
+# enable pulseaudio
+ln -s /usr/lib/systemd/user/pulseaudio.socket /home/audioclient/.config/systemd/user/sockets.target.wants
 ln -s /usr/lib/systemd/user/pulseaudio.service /home/audioclient/.config/systemd/user/default.target.wants/pulseaudio.service
+# ensure, that pulseaudio always restarts
+mkdir -p /home/audioclient/.config/systemd/user/pulseaudio.service.d
+cp contrib/pulseaudio-restart.conf /home/audioclient/.config/systemd/user/pulseaudio.service.d
+# cp contrib/pulseaudio-custom.conf /home/audioclient/.config/systemd/user/pulseaudio.service.d
+# you can check the used settings with "systemctl --user show pulseaudio.service"
+
+# enable bluetooth-monitor
 cp contrib/bluetooth-monitor.service /home/audioclient/.config/systemd/user
-cp contrib/pulseaudio-audioclient.service /home/audioclient/.config/systemd/user
-#ln -s /home/audioclient/.config/systemd/user/pulseaudio-audioclient.service /home/audioclient/.config/systemd/user/default.target.wants/pulseaudio-audioclient.service
-#ln -s /home/audioclient/.config/systemd/user/bluetooth-monitor.service /home/audioclient/.config/systemd/user/default.target.wants/bluetooth-monitor.service
+ln -s /home/audioclient/.config/systemd/user/bluetooth-monitor.service /home/audioclient/.config/systemd/user/default.target.wants/bluetooth-monitor.service
+
+# fix file permissions
 chown -R audioclient:audioclient /home/audioclient/
+
 
 # now we can use system
 loginctl enable-linger audioclient
