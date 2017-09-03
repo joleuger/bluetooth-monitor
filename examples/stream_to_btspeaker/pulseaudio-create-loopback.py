8431becdc2b0492bf39c9b344ab5c43664c2d6eb
Johannes Leupolz <johannes.leupolz@googlemail.com>
2017-09-03 17:45:57 +0200

    fixed create-loopback script

--- examples/stream_to_btspeaker/pulseaudio-create-loopback.py
+++ examples/stream_to_btspeaker/pulseaudio-create-loopback.py
@@ -8,19 +8,24 @@ loopbackDevice=None
 
 def signal_term_handler(signal, frame):
   print("Received SIGTERM. Close loopback")
+  print(loopbackDevice)
   if loopbackDevice != None:
      subprocess.run(["/usr/bin/pactl", "unload-module", loopbackDevice])
   sys.exit(0)
 
 signal.signal(signal.SIGTERM, signal_term_handler)
+signal.signal(signal.SIGINT, signal_term_handler)
 
 btmac=sys.argv[1]
 
 print("bluetooth address to create loopback for: "+btmac)
 
-createLoopbackProcess = subprocess.Popen(["/usr/bin/pactl", "load-module","module-loopback",'source="snapclient.monitor"','sink=bluez_sink.'+btmac+'.a2dp_sink'],stdout=subprocess.PIPE)
+createLoopbackProcess = subprocess.Popen(["/usr/bin/pactl", "load-module","module-loopback",'source="snapcast.monitor"','sink=bluez_sink.'+btmac+'.a2dp_sink'],stdout=subprocess.PIPE)
+createLoopbackProcessOutput,createLoopbackProcessErr=createLoopbackProcess.communicate()
 if createLoopbackProcess.returncode == 0:
-  loopbackDevice=createLoopbackProcess.stdout.strip()
+  loopbackDevice=createLoopbackProcessOutput.strip()
+
+print("pactl returncode: "+str(createLoopbackProcess.returncode))
 
 while True:
   time.sleep(10)
